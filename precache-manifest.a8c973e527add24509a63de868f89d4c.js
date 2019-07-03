f4247c02a839aed883996bc56c735a1d80a508ee
h2ash <hun22ash@gmail.com>
2019-07-03 16:22:47 +0300

    Updates

---
+++ precache-manifest.a8c973e527add24509a63de868f89d4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618b909430e9fbd1427b04d5f2e4d0e9",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "fa3630b0e7c0bf1f467c",
+    "url": "/react_static-list-of-posts/static/css/main.54e20f30.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "fa3630b0e7c0bf1f467c",
+    "url": "/react_static-list-of-posts/static/js/main.a3f7bbb6.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
--- src/components/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function User(props) {
-  return (
-    <div className='article-author'>author: {props.user.name} email: {props.user.email} from:{props.user.address.city}</div>
-    )
-}
-
-export default User
